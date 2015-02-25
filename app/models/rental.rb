ec72035138dd693a749f874ff682988d44a5a9f9
frederic lambrechts <f.lambrechts@gmail.com>
2015-02-25 17:18:05 +0100

    add show rental page

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,7 +2,7 @@ class Rental < ActiveRecord::Base
 
   RENTAL_TYPE = ["house","appt"]
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "300x300>", thumb: "100x100>" }, :default_url => ":style/missing.jpg"
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
