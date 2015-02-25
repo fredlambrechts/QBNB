d9bd619b2b226b6506178ea35f0d259c8c1d2ec2
frederic lambrechts <f.lambrechts@gmail.com>
2015-02-25 13:20:01 +0100

    first filter on index page & logo

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,8 +1,21 @@
 class Rental < ActiveRecord::Base
+
   RENTAL_TYPE = ["house","appt"]
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
+
+  def self.appts
+    where(rental_type: "appt")
+  end
+
+  def self.houses
+    where(rental_type: "house")
+  end
+
+# For later: rewrite self selection methods into scopes
+  # scope :rental_type, -> (rental_type) { where rental_type: rental_type }
+  # scope :location, -> (address_city) { where address_city: address_city }
 end
