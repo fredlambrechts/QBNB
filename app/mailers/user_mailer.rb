3bf75483f0a1b62c4060cbe950704c9dfa64d007
frederic lambrechts <f.lambrechts@gmail.com>
2015-02-26 16:50:26 +0100

    send availability request to owner

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'f.lambrechts@gmail.com'
+  default from: 'subscribe@qbnb.com'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -10,4 +10,12 @@ class UserMailer < ApplicationMailer
 
     mail(to: @user.email, subject: 'Welcome to QBNB')
   end
+
+  def availability_request(owner, booking)
+    @owner = owner
+    @booking = booking
+
+    mail(to: @owner.email, subject: 'Someone wants to know if your rental is avialable!')
+  end
+
 end
