5138a1a7861bf0d5fb74d9f8024d0e75f76415d8
frederic lambrechts <f.lambrechts@gmail.com>
2015-02-26 15:33:10 +0100

    confirmation email at user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_create :send_welcome_email
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :rentals
   has_many :bookings
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  private
+
+    def send_welcome_email
+      UserMailer.welcome(self).deliver
+    end
 
 end
